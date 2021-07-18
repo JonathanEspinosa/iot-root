c7c62ec6d95995b1eea0c70d8a46d71162b98e7c
Jessica Pinta <47082485+JessicaEstefaniaPinta@users.noreply.github.com>
2021-07-18 09:30:32 -0500

    Proyecto-PostgreSQL

---
+++ TesisSonoff/Modulos/Gestion/models.py
@@ -0,0 +1,24 @@
+
+
+from django.db import models
+
+# Create your models here.
+class Persona(models.Model):
+    idpersona=models.CharField(max_length=3, primary_key=True)
+    nombre = models.CharField(max_length=10)
+    apellido = models.CharField(max_length=10)
+    
+    def nombreCompleto():
+        txt = "{0} {1},{2}"
+        return txt.format(Self.nombre, Self.persona)
+    def _str_(Self):
+       return "{0}".format(Self.nombre)
+
+class Salas(models.Model):
+    idsala=models.CharField(max_length=3, primary_key=True)
+    nombre = models.CharField(max_length=10)
+    persona = models.ForeignKey(Persona, null=False, blank=False, on_delete=models.CASCADE) 
+    def _str_(Self):
+       return "{0}".format(Self.nombre)
+    
+
