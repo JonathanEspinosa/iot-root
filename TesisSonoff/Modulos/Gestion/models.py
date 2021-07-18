aeee5bebc55fb73add8f61d89a859f2817662fab
Jessica Pinta <47082485+JessicaEstefaniaPinta@users.noreply.github.com>
2021-07-18 11:00:07 -0500

    1

--- TesisSonoff/Modulos/Gestion/models.py
+++ TesisSonoff/Modulos/Gestion/models.py
@@ -8,17 +8,20 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=10)
     apellido = models.CharField(max_length=10)
     
-    def nombreCompleto():
+    
+    def nombreCompleto(self):
         txt = "{0} {1},{2}"
-        return txt.format(self.nombre, self.persona)
-    def _str_(Self):
-       return "{0}".format(Self.nombre)
+        return txt.format(self.nombre, self.apellido)
+        
+    def _str_(self):
+       return self.nombreCompleto()
+
 
 class Salas(models.Model):
     idsala=models.CharField(max_length=3, primary_key=True)
     nombre = models.CharField(max_length=10)
     persona = models.ForeignKey(Persona, null=False, blank=False, on_delete=models.CASCADE) 
-    def _str_(Self):
-       return "{0}".format(Self.nombre)
+    def _str_(self):
+       return "{0} ({1})".format(self.nombre, self.persona)
     
 
