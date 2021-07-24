b1c543d90118a74d5d67f1741a089aacae7d1a2b
Jessica Pinta <47082485+JessicaEstefaniaPinta@users.noreply.github.com>
2021-07-24 11:42:03 -0500

    groupcode

--- TesisSonoff/Modulos/Gestion/views.py
+++ TesisSonoff/Modulos/Gestion/views.py
@@ -1,25 +1,36 @@
+from io import StringIO
 import json
 from json.encoder import JSONEncoder
+from typing import List
 from django.db.models.fields import CommaSeparatedIntegerField
 from django.http.response import JsonResponse
 from django.utils.decorators import method_decorator
+from django.utils.regex_helper import Group
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from .models import GROUP, models
+from .models import GROUP, models 
 import json
 # Create your views here.
 class GroupView(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
          return super().dispatch(request, *args, **kwargs)
-    def  get(self,request):
-         groups= list(GROUP.objects.values()) 
-         
-         if len(groups)>0:
-             datos={'message':"Success",'groups':groups}
+    def  get(self, request, groupcode=0):
+         if (groupcode>0):
+               groups = list(GROUP.objects.filter(groupcode=groupcode).values())
+               if len(groups) > 0:
+                   Group=groups[0]
+                   datos={'message':"Success",'groups':Group}
+               else: 
+                   datos={'message':"Groups not found..."}
+               return JsonResponse(datos)
          else:
-             datos={'message':"Groups not found..."}
-         return JsonResponse(datos)  
+               groups = list(GROUP.objects.values()) 
+               if len(groups) > 0:
+                    datos={'message':"Success",'groups':groups}
+               else:
+                    datos={'message':"Groups not found..."}
+               return JsonResponse(datos)
     def  post(self,request): 
          #print(request.body)#enviado el json para que se registre
          jd=json.loads(request.body)
