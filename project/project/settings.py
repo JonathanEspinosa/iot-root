c50b96d21dbf766dd5927ba06912b055680bca87
JEEM\Jonathan <jonathanedu@hotmail.es>
2021-08-04 00:48:00 -0500

    asd
    asd

--- project/project/settings.py
+++ project/project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'Tesis',
         'USER': 'postgres',
-        'PASSWORD': 'tesis',
+        'PASSWORD': 'tesias',
         'HOST': 'localhost',
         'DATABASE_PORT': '5432',
     }
